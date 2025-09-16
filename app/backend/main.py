1c57b10ee3c38b926b31a4607c555f64ec73fd96
Abhishree Bhardwaj <67201467+AbhishreeBh0208@users.noreply.github.com>
2025-09-17 04:45:27 +0530

    Update main.py

--- app/backend/main.py
+++ app/backend/main.py
@@ -435,10 +435,5 @@ async def launch_mission(params: dict):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host = "127.0.0.1", 
-        port=8000,
-        reload = True,
-        lifespan = "off"
-    )
+    port = int(os.environ.get("PORT", 8000))  # Render gives port via env var
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
